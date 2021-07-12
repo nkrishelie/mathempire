9a28f9808572fc13659297191a2a0332884d319c
nkrishelie <60033261+nkrishelie@users.noreply.github.com>
2021-07-12 08:08:39 +0300

    Add files via upload

---
+++ 250/makeall.sh
@@ -0,0 +1,9 @@
+makeindex 250leçons.idx
+makeglossaries 250leçons.glo
+makeglossaries 250leçons.glsdefs
+pdflatex 250leçons.tex
+makeindex 250leçons.idx
+makeglossaries 250leçons.glo
+makeglossaries 250leçons.glsdefs
+pdflatex 250leçons.tex
+pdflatex 250leçons.tex
